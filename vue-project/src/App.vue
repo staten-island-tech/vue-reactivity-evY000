4b1eefb3c955526a67fc5b2b8e195f52137158b3
evY000 <000evan001@gmail.com>
2024-02-15 13:30:51 -0500

    qasdfghjm

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,34 +1,41 @@
 <script setup>
-import Cards from '@/components/cards.vue'
+ import Cards from '@/components/cards.vue' 
+const CardList = [
+{Name :"Lebron James", Image:"https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"},
+ {Name:"Giannis Antetokounmpo", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203507.png"},
+ {Name:"Luka Doncic" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1629029.png"},
+ {Name:"Joel Embiid" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203954.png"},
+ {Name:"Kevin Durant", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/201142.png"},
+ {Name:"Tyrese Haliburton", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1630169.png"},
+ {Name:"Shai Gilgeous-Alexander", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1628983.png"},
+ {Name:"Damian Lilliard", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203081.png"},
+ {Name:"Jayson Tatum", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1628369.png"},
+ {Name:"Nikola Jokic", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203999.png"},
+ {Name:"Bam Adebayo", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1628389.png"},
+ {Name:"Devin Booker" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1626164.png"},
+ {Name:"Paolo Banchero" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1631094.png"},
+ {Name:"Stephen Curry", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/201939.png"},
+ {Name:"Scottie Barnes" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1630567.png"},
+ {Name:"Anthony Davis", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203076.png"},
+ {Name:"Jaylen Brown", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1627759.png"},
+ {Name:"Anthony Edwards" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1630162.png"},
+ {Name:"Jalen Brunson", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1628973.png"},
+ {Name:"Paul George" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/202331.png"},
+ {Name:"Tyrese Maxey" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1630178.png"},
+ {Name:"Kawhi Leonard", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/202695.png"},
+ {Name:"Donovan Mitchell" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1628378.png"},
+ {Name:"Karl Anthony Towns", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1626157.png"},
+ {Name:"Julius Randle", Image:"https://cdn.nba.com/headshots/nba/latest/260x190/203944.png"},
+ {Name:"Trae Young" ,Image:"https://cdn.nba.com/headshots/nba/latest/260x190/1629027.png"},
+]
+CardList.forEach(card => {console.log(card.Name)
+    
+});
 </script>
 <template>
+<body>
+    
+</body>
 <div class="Card">
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards>
-<Cards Name="Lebron James" Image="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png"> </Cards></div>
+</div>
 </template>
--- vue-project/src/components/cards.vue
+++ vue-project/src/components/cards.vue
@@ -1,11 +1,10 @@
-
 <template>
-    <div> <h1 class="text">{{ Name }}<img class="img" :src="Image"> </h1></div>
+    <div class="Card"> <h1 class="text">{{ Name }}<img class="img" :src="Image"> <button class="btn" type="button">Select</button>  </h1></div>
 </template>
 <script setup>
 const props = defineProps({
    Name: String,
-    Image : String
+    Image : String,
 })
 </script>
 <style lang="scss" scoped>
