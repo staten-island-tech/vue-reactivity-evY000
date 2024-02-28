e04518f2437c3e6bda26a913b0ae545ca621ff31
evY000 <000evan001@gmail.com>
2024-02-28 13:13:24 -0500

    asdf

--- vue-project/src/components/cards.vue
+++ vue-project/src/components/cards.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="Card"> <h1 class="text">{{ Name }}<img class="img" :src="Image"> <button class="btn" type="button">Select</button>  </h1></div>
+    <div class="Card"> <h1 class="text">{{ players.Name }}<img class="img" :src="players.Image"> <button class="btn" type="button">Select</button>  </h1></div>
 </template>
 <script setup>
 const props = defineProps({
-   Name: String,
-    Image : String,
+    players: Object
 })
 </script>
 <style lang="scss" scoped>
 </style>
+<!-- V-for the cards
+button
+user input
+turn mechanic
+position mechanic -->
